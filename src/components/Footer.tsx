1522efbd7bf9731f0f98cfe38b3fcaff1bf29b84
Michel Policeno <michel.policeno@hotmail.com>
2025-10-27 16:58:55 -0300

    refactor: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import "../styles/Footer.css";
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} TodoApp — Desenvolvido por Michel</p>
+      <p>© {new Date().getFullYear()} TodoApp — Desenvolvido por Michel Policeno</p>
     </footer>
   );
 };
