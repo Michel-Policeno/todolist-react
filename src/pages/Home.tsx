bcb3b4876f189726734ab20a8686e52a003afce4
Michel Policeno <michel.policeno@hotmail.com>
2025-12-03 12:50:58 -0300

    refactor:  homepage add card movies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,13 +14,18 @@ const Home: React.FC = () => {
 
       <main className="home-container">
         <div className="home-card" onClick={() => navigate("/tasks")}>
-          <h2>Tasks</h2>
-          <p>Gerencie suas tarefas.</p>
+          <h2>Tarefas</h2>
+          <p>Gerencie suas lista de tarefas</p>
         </div>
 
         <div className="home-card" onClick={() => navigate("/foods")}>
           <h2>Receitas</h2>
-          <p>Escolha sua receita.</p>
+          <p>Escolha sua próxima receita</p>
+        </div>
+      
+          <div className="home-card" onClick={() => navigate("/movies")}>
+          <h2>Filmes e Series</h2>
+          <p>Veja onde assistir seu filme ou serie favorita</p>
         </div>
       </main>
 
