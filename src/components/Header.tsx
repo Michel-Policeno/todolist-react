533efe04ba49bbf3545ac2c0e98a29356cc8d884
Michel Policeno <michel.policeno@hotmail.com>
2025-12-03 12:50:34 -0300

    refactor: add btn movies and remove onSearch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 import "../styles/Header.css";
 
@@ -7,55 +7,34 @@ interface HeaderProps {
   onSearch: (query: string) => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ onSearch }) => {
+const Header: React.FC<HeaderProps> = () => {
   const navigate = useNavigate();
   const { logout } = useAuth();
-  const [search, setSearch] = useState("");
-  const [theme, setTheme] = useState("light");
-
-  const handleThemeToggle = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    document.body.setAttribute("data-theme", newTheme);
-  };
 
   return (
     <header className="header">
       <button onClick={() => navigate("/home")} className="btn-home">
-          Home
-        </button>
+        Home
+      </button>
 
-          <button onClick={() => navigate("/tasks")} className="btn-tarefas">
-          Tarefas
-        </button>
+      <button onClick={() => navigate("/tasks")} className="btn-tarefas">
+        Tarefas
+      </button>
 
-         <button onClick={() => navigate("/foods")} className="btn-receitas">
-          Receitas
-        </button>
- 
+      <button onClick={() => navigate("/foods")} className="btn-receitas">
+        Receitas
+      </button>
 
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Buscar tarefas..."
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-            onSearch(e.target.value);
-          }}
-        />
-      </div>
+      <button onClick={() => navigate("/movies")} className="btn-filmes">
+        Filmes/Series
+      </button>
 
       <div className="user-controls">
-      
-        {/* <span className="username">Olá, Michel</span> */}
-        {/* <button onClick={handleThemeToggle} className="btn-theme">
-          {theme === "light" ? "🌙" : "☀️"}
-        </button> */}
         <button onClick={logout} className="btn-logout">
           Sair
         </button>
       </div>
+    
     </header>
   );
 };
