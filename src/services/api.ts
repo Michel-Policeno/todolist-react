440fa52772300c5be15a8d730f9ef97c31ea18d3
Michel Policeno <michel.policeno@hotmail.com>
2025-12-09 17:42:53 -0300

    add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const api = axios.create({
-  baseURL: `${API_BASE}/api`
+  baseURL: `${API_BASE_URL}/api`
   
 });
 
